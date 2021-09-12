dc68beda3feb498127c41ccc5740159450c03c9e
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-12 17:22:53 +0900

    Initial commit

---
+++ springboot/src/main/kotlin/app/vendingmachine/VendingMachineApplication.kt
@@ -0,0 +1,11 @@
+package app.vendingmachine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VendingMachineApplication
+
+fun main(args: Array<String>) {
+	runApplication<VendingMachineApplication>(*args)
+}
---
+++ springboot/src/test/kotlin/app/vendingmachine/VendingMachineApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.vendingmachine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VendingMachineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
