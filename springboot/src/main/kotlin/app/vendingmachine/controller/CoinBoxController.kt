09427e99c9634bc59d10caeefa14f4c525d83d94
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-13 12:40:03 +0900

    update: coin_boxテーブルのUPDATE処理を実装

--- springboot/src/main/kotlin/app/vendingmachine/controller/CoinBoxController.kt
+++ springboot/src/main/kotlin/app/vendingmachine/controller/CoinBoxController.kt
@@ -3,6 +3,7 @@ package app.vendingmachine.controller
 import app.vendingmachine.mapper.CoinBoxMapper
 import app.vendingmachine.model.CoinBox
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,6 +14,11 @@ class CoinBoxController(val coinBoxMapper: CoinBoxMapper) {
     @GetMapping("")
     fun findAll():CoinBox{
         return coinBoxMapper.findAll()
+    }
+
+    @PutMapping("")
+    fun update(coinBox: CoinBox):Boolean{
+        return coinBoxMapper.update(coinBox)
     }
 
 }
--- springboot/src/main/kotlin/app/vendingmachine/mapper/CoinBoxMapper.kt
+++ springboot/src/main/kotlin/app/vendingmachine/mapper/CoinBoxMapper.kt
@@ -3,6 +3,7 @@ package app.vendingmachine.mapper
 import app.vendingmachine.model.CoinBox
 import org.apache.ibatis.annotations.Mapper
 import org.apache.ibatis.annotations.Select
+import org.apache.ibatis.annotations.Update
 
 @Mapper
 interface CoinBoxMapper {
@@ -11,4 +12,16 @@ interface CoinBoxMapper {
         SELECT * FROM coin_box
     """)
     fun findAll():CoinBox
+
+    @Update("""
+        UPDATE coin_box
+        SET deposit=#{deposit},
+            left_10=#{left10},
+            left_50=#{left50},
+            left_100=#{left100},
+            left_500=#{left500},
+            left_1000=#{left1000}
+        WHERE id=0
+    """)
+    fun update(coinBox: CoinBox):Boolean
 }
