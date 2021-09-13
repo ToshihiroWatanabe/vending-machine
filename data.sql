e0a1cfcb25997b1f0f3bd414e0286b03eaec8de1
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-13 09:16:39 +0900

    update: DDLの修正とお釣りの初期データの挿入SQLを作成

---
+++ data.sql
@@ -0,0 +1,18 @@
+INSERT INTO coin_box(
+    id,
+    deposit,
+    left_10,
+    left_50,
+    left_100,
+    left_500,
+    left_1000
+)
+VALUES(
+    0,
+    0,
+    128,
+    128,
+    128,
+    128,
+    128
+);
--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 -- 製品テーブル
 CREATE TABLE IF NOT EXISTS products(
-    -- UUID
+    -- 製品ID
     id BINARY(16) PRIMARY KEY,
     -- 製品名
     name VARCHAR(100),
@@ -18,27 +18,40 @@ CREATE TABLE IF NOT EXISTS products(
 
 -- 在庫テーブル
 CREATE TABLE IF NOT EXISTS stocks(
+    -- 在庫ID
     id INTEGER PRIMARY KEY,
+    -- 製品ID
     product_id BINARY(16) NOT NULL,
+    -- 在庫数
     quantity INTEGER NOT NULL,
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 -- 売上履歴テーブル
 CREATE TABLE IF NOT EXISTS sales_history(
-    -- UUID
+    -- 履歴ID
     id BINARY(16) PRIMARY KEY,
+    -- 購入時刻
     purchase_time DATETIME NOT NULL,
+    -- 製品ID
     product_id BINARY(16) NOT NULL,
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 -- 投入金額とお釣りテーブル
-CREATE TABLE IF NOT EXISTS coin_slot(
+CREATE TABLE IF NOT EXISTS coin_box(
+    -- ID
+    id INTEGER PRIMARY KEY,
+    -- 投入金額
     deposit INTEGER NOT NULL,
+    -- 10円玉の残り枚数
     left_10 INTEGER NOT NULL,
+    -- 50円玉の残り枚数
     left_50 INTEGER NOT NULL,
+    -- 100円玉の残り枚数
     left_100 INTEGER NOT NULL,
+    -- 500円玉の残り枚数
     left_500 INTEGER NOT NULL,
+    -- 1000円玉の残り枚数
     left_1000 INTEGER NOT NULL
 );
