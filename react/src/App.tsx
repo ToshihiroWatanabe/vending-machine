7190c57f14c3365f9fc4d9928a48ba392b5d4780
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-14 09:23:37 +0900

    update: ページ表示時にコインボックス情報を取得

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/coin-box/").then((res) => {
+      console.log(res);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>自動販売機アプリ</h1>
     </div>
   );
 }
