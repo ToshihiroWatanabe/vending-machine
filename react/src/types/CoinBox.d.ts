586f8749412d494e447873c39db77e8c596d49e8
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-22 19:12:21 +0900

    update: CoinBoxとStockを型定義ファイル化

---
+++ react/src/types/CoinBox.d.ts
@@ -0,0 +1,8 @@
+export type CoinBox = {
+  deposit: number;
+  left10: number;
+  left50: number;
+  left100: number;
+  left500: number;
+  left1000: number;
+};
---
+++ react/src/types/Stock.d.ts
@@ -0,0 +1,7 @@
+export type Stock = {
+  id: number;
+  name: string;
+  temperature: string;
+  price: number;
+  quantity: number;
+};
