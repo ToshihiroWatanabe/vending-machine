b5a166dbc607b14c7cbbd2415acc9922edea8716
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-22 17:43:45 +0900

    add: 管理者ページを追加

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,141 +1,20 @@
-import React, { useEffect, useState } from "react";
-import CoinBoxService from "services/CoinBox.service";
-import "./App.css";
+import Admin from "pages/Admin";
+import VendingMachine from "pages/VendingMachine";
+import { Route, Switch } from "react-router-dom";
 
-type CoinBox = {
-  deposit: number;
-  left10: number;
-  left50: number;
-  left100: number;
-  left500: number;
-  left1000: number;
-};
-
-type Stock = {
-  id: number;
-  name: string;
-  temperature: string;
-  price: number;
-  quantity: number;
-};
-
-function App() {
-  const [coinBox, setCoinBox] = useState<CoinBox>();
-  const [stocks, setStocks] = useState<Stock[]>([
-    { id: 1, name: "商品1", temperature: "cold", price: 140, quantity: 24 },
-    { id: 2, name: "商品2", temperature: "hot", price: 120, quantity: 24 },
-  ]);
-
-  useEffect(() => {
-    CoinBoxService.get().then((res: any) => {
-      console.log(res.data);
-      setCoinBox(res.data);
-    });
-  }, []);
-
-  const onInsertButtonClick = (value: number) => {
-    CoinBoxService.insert(value).then((res: any) => {
-      console.log(res.data);
-      setCoinBox(res.data);
-      console.log(value + "円が投入されました");
-    });
-  };
-
-  const onReleaseButtonClick = () => {
-    CoinBoxService.release().then((res: any) => {
-      console.log(res.data);
-      setCoinBox(res.data);
-      console.log("おつり返却");
-    });
-  };
-
+const App = () => {
   return (
     <div className="App">
-      <h1>自動販売機アプリ</h1>
-      {stocks.map((stock) => {
-        return (
-          <button key={stock.id}>
-            {stock.name}
-            <br />
-            {stock.temperature === "hot"
-              ? "あたたかい"
-              : stock.temperature === "cold"
-              ? "つめたい"
-              : ""}
-            <br />
-            {stock.quantity !== 0 ? stock.price + "円" : "売り切れ"}
-          </button>
-        );
-      })}
-      <p>投入金額: {coinBox?.deposit}円</p>
-      <div>
-        <button
-          onClick={() => {
-            onInsertButtonClick(10);
-          }}
-        >
-          10円玉を入れる
-        </button>
-        <button
-          onClick={() => {
-            onInsertButtonClick(50);
-          }}
-        >
-          50円玉を入れる
-        </button>
-        <button
-          onClick={() => {
-            onInsertButtonClick(100);
-          }}
-        >
-          100円玉を入れる
-        </button>
-        <button
-          onClick={() => {
-            onInsertButtonClick(500);
-          }}
-        >
-          500円玉を入れる
-        </button>
-        <button
-          onClick={() => {
-            onInsertButtonClick(1000);
-          }}
-        >
-          1000円札を入れる
-        </button>
-      </div>
-      <button
-        onClick={() => {
-          onReleaseButtonClick();
-        }}
-      >
-        おつり・返却
-      </button>
-      <table>
-        <tr>
-          <th>10円玉</th>
-          <th>残り{coinBox?.left10}枚</th>
-        </tr>
-        <tr>
-          <th>50円玉</th>
-          <th>残り{coinBox?.left50}枚</th>
-        </tr>
-        <tr>
-          <th>100円玉</th>
-          <th>残り{coinBox?.left100}枚</th>
-        </tr>
-        <tr>
-          <th>500円玉</th>
-          <th>残り{coinBox?.left500}枚</th>
-        </tr>
-        <tr>
-          <th>1000円札</th>
-          <th>残り{coinBox?.left1000}枚</th>
-        </tr>
-      </table>
+      <Switch>
+        <Route exact path="/">
+          <VendingMachine />
+        </Route>
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ react/src/pages/Admin.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const Admin = () => {
+  return (
+    <Fragment>
+      <h1>自動販売機アプリ - 管理者ページ</h1>
+    </Fragment>
+  );
+};
+
+export default Admin;
