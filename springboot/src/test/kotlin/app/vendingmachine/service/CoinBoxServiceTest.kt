eb180c5d67ec43a2f9b5a57dd5fc317e61efb4b0
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-22 15:08:48 +0900

    Create CoinBoxServiceTest.kt

---
+++ springboot/src/test/kotlin/app/vendingmachine/service/CoinBoxServiceTest.kt
@@ -0,0 +1,26 @@
+package app.vendingmachine.service
+
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@DisplayName("CoinBoxServiceのテスト")
+class CoinBoxServiceTest {
+    @Autowired
+    lateinit var coinBoxService: CoinBoxService
+
+    @Test
+    fun calcChange1000(){
+        val array:Array<Int> = arrayOf(1, 0, 0, 0, 0, 0)
+        assertArrayEquals(array, coinBoxService.calcChange(1000))
+    }
+
+    @Test
+    fun calcChange500(){
+        val array:Array<Int> = arrayOf(0, 1, 0, 0, 0, 0)
+        assertArrayEquals(array, coinBoxService.calcChange(500))
+    }
+}
