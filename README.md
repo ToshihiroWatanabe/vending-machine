6f8abf3e41ce8e1ae71f19a787d69243a4f3f737
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-09-23 18:22:41 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # vending-machine
+
+自動販売機をWebアプリで再現します。
+
+https://vending--machine.herokuapp.com/
+
+# 仕様
+## 使えるお金
+
+- 10円玉
+- 50円玉
+- 100円玉
+- 500円玉
+- 1000円札
+
+# 使用技術
+
+- React
+- TypeScript
+- Spring Boot
+- Kotlin
+- MyBatis
+- MySQL
+
+# データベース
+
+テーブルを作成するSQLファイルはこちらです。
+[springboot\src\test\resources\schema.sql](springboot\src\test\resources\schema.sql)
--- springboot/src/main/kotlin/app/vendingmachine/service/CoinBoxService.kt
+++ springboot/src/main/kotlin/app/vendingmachine/service/CoinBoxService.kt
@@ -65,4 +65,20 @@ class CoinBoxService @Autowired constructor(val coinBoxMapper: CoinBoxMapper) {
     fun release(): Boolean {
         return coinBoxMapper.release()
     }
+
+    /**
+     * お釣りの金額から硬貨や紙幣の枚数を計算します。
+     * できるだけ枚数が少なくなるようにします。
+     */
+    fun calcChange(change: Int): Array<Int> {
+        val moneyList = listOf(1000, 500, 100, 50, 10)
+        var moneyCount: Array<Int> = arrayOf()
+        var changeLeft = change
+        moneyList.forEach { money ->
+            val changeMoney = changeLeft / money
+            moneyCount += changeMoney
+            changeLeft %= money
+        }
+        return moneyCount
+    }
 }
